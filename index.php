630b5a9e3506ac9a0473b2640a238167bf3f1f1c
Sourab Hossain <sourabhossain.fci@gmail.com>
2021-01-26 19:43:00 +0600

    fcc good looking

--- index.php
+++ index.php
@@ -11,144 +11,146 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
+    <div class="container-fluid mt-4 mb-4 p-4">
+        <div class="card">
+            <h5 class="card-header">FCC: Survey Form</h5>
 
-    <form action="" method="post" id="register" role="form" autocomplete="off">
-        <div class="container main mt-4 mb-4 p-4">
-            <h4 class="display-4 text-center">FCC: Survey Form</h4>
-            <hr>
+            <div class="card-body">
+                <form action="" method="post" id="register" role="form" autocomplete="off">
+                    <input type="hidden" id="id" value="<?= !empty($_GET['update'])? $_GET['id'] : "" ?>">
 
-            <input type="hidden" id="id" value="<?= !empty($_GET['update'])? $_GET['id'] : "" ?>">
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" class="form-control" name="name" id="name" required oninvalid="this.setCustomValidity('Please enter your name')" oninput="setCustomValidity('')">
+                    </div>
 
-            <div class="form-group">
-                <label for="name">Name</label>
-                <input type="text" class="form-control" name="name" id="name" required oninvalid="this.setCustomValidity('Please enter your name')" oninput="setCustomValidity('')">
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" name="email" id="email" required oninvalid="this.setCustomValidity('Please enter your email')" oninput="setCustomValidity('')">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="Age">Age</label>
+                        <input type="number" class="form-control" name="age" id="age" required oninvalid="this.setCustomValidity('Please enter your age')" oninput="setCustomValidity('')">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="current_role">Which option best describes your current role?</label>
+                        <select class="form-control" id="current_role" name="current_role" required oninvalid="this.setCustomValidity('Please enter your current role')"
+                            oninput="setCustomValidity('')">
+                            <option value="">Select current role</option>
+                            <option value="student">Student</option>
+                            <option value="job">Full Time Job</option>
+                            <option value="learner">Full Time Learner</option>
+                            <option value="not_say">Prefer not to say</option>
+                            <option value="other">Other</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="recommend_friend">Would you recommend freeCodeCamp to a friend?</label>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="recommend_friend" id="definitely" value="definitely" required oninvalid="this.setCustomValidity('Please enter your recommend friend')" oninput="setCustomValidity('')">
+                            <label class="form-check-label" for="definitely">Definitely</label>
+                        </div>
+
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="recommend_friend" id="maybe" value="maybe">
+                            <label class="form-check-label" for="maybe">Maybe</label>
+                        </div>
+
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="recommend_friend" id="not_sure" value="not_sure">
+                            <label class="form-check-label" for="not_sure">Not sure</label>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="favorite_feature">What is your favorite feature of freeCodeCamp?</label>
+                        <select class="form-control" id="favorite_feature" name="favorite_feature" required oninvalid="this.setCustomValidity('Please enter your favorite feature')" oninput="setCustomValidity('')">
+                            <option value="">Select an option</option>
+                            <option value="challenges">Challenges</option>
+                            <option value="projects">Projects</option>
+                            <option value="community">Community</option>
+                            <option value="open_source">Open Source</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="see_improved">What would you like to see improved? (<small>Check all that apply</small>)</label>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" id="front_end_projects" value="front_end_projects" name="see_improved">
+                            <label class="form-check-label" for="front_end_projects">Front-end Projects</label>
+                        </div>
+
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" id="back_end_projects" value="back_end_projects" name="see_improved">
+                            <label class="form-check-label" for="back_end_projects">Back-end Projects</label>
+                        </div>
+
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" id="data_visualization" value="data_visualization" name="see_improved">
+                            <label class="form-check-label" for="data_visualization">Data Visualization</label>
+                        </div>
+
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" id="challenges" value="challenges" name="see_improved">
+                            <label class="form-check-label" for="challenges">Challenges</label>
+                        </div>
+
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" id="open_source_community" value="open_source_community" name="see_improved">
+                            <label class="form-check-label" for="open_source_community">Open Source Community</label>
+                        </div>
+
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" id="gitter" value="gitter" name="see_improved">
+                            <label class="form-check-label" for="gitter">Gitter help rooms</label>
+                        </div>
+
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" id="videos" value="videos" name="see_improved">
+                            <label class="form-check-label" for="videos">Videos</label>
+                        </div>
+
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" id="city_meetups" value="city_meetups" name="see_improved">
+                            <label class="form-check-label" for="city_meetups">City Meetups</label>
+                        </div>
+
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" id="wiki" value="wiki" name="see_improved">
+                            <label class="form-check-label" for="wiki">Wiki</label>
+                        </div>
+
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" id="forum" value="forum" name="see_improved">
+                            <label class="form-check-label" for="forum">Forum</label>
+                        </div>
+
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" id="additional_courses" value="additional_courses" name="see_improved">
+                            <label class="form-check-label" for="additional_courses">Additional Courses</label>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="suggestions">Any comments or suggestions?</label>
+                        <textarea class="form-control" id="suggestions" rows="5" placeholder="Enter your comment here..." required oninvalid="this.setCustomValidity('Please enter your suggestions')" oninput="setCustomValidity('')"></textarea>
+                    </div>
+
+                    <input type="submit" class="btn btn-primary" value="<?= !empty($_GET['update'])? 'Update Data' : 'Save Data'; ?>">
+                </form>
             </div>
-
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input type="email" class="form-control" name="email" id="email" required oninvalid="this.setCustomValidity('Please enter your email')" oninput="setCustomValidity('')">
-            </div>
-
-            <div class="form-group">
-                <label for="Age">Age</label>
-                <input type="number" class="form-control" name="age" id="age" required oninvalid="this.setCustomValidity('Please enter your age')" oninput="setCustomValidity('')">
-            </div>
-
-            <div class="form-group">
-                <label for="current_role">Which option best describes your current role?</label>
-                <select class="form-control" id="current_role" name="current_role" required oninvalid="this.setCustomValidity('Please enter your current role')"
-                    oninput="setCustomValidity('')">
-                    <option value="">Select current role</option>
-                    <option value="student">Student</option>
-                    <option value="job">Full Time Job</option>
-                    <option value="learner">Full Time Learner</option>
-                    <option value="not_say">Prefer not to say</option>
-                    <option value="other">Other</option>
-                </select>
-            </div>
-
-            <div class="form-group">
-                <label for="recommend_friend">Would you recommend freeCodeCamp to a friend?</label>
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" name="recommend_friend" id="definitely" value="definitely" required oninvalid="this.setCustomValidity('Please enter your recommend friend')" oninput="setCustomValidity('')">
-                    <label class="form-check-label" for="definitely">Definitely</label>
-                </div>
-
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" name="recommend_friend" id="maybe" value="maybe">
-                    <label class="form-check-label" for="maybe">Maybe</label>
-                </div>
-
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" name="recommend_friend" id="not_sure" value="not_sure">
-                    <label class="form-check-label" for="not_sure">Not sure</label>
-                </div>
-            </div>
-
-            <div class="form-group">
-                <label for="favorite_feature">What is your favorite feature of freeCodeCamp?</label>
-                <select class="form-control" id="favorite_feature" name="favorite_feature" required oninvalid="this.setCustomValidity('Please enter your favorite feature')" oninput="setCustomValidity('')">
-                    <option value="">Select an option</option>
-                    <option value="challenges">Challenges</option>
-                    <option value="projects">Projects</option>
-                    <option value="community">Community</option>
-                    <option value="open_source">Open Source</option>
-                </select>
-            </div>
-
-            <div class="form-group">
-                <label for="see_improved">What would you like to see improved? (<small>Check all that apply</small>)</label>
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="front_end_projects" value="front_end_projects" name="see_improved">
-                    <label class="form-check-label" for="front_end_projects">Front-end Projects</label>
-                </div>
-
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="back_end_projects" value="back_end_projects" name="see_improved">
-                    <label class="form-check-label" for="back_end_projects">Back-end Projects</label>
-                </div>
-
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="data_visualization" value="data_visualization" name="see_improved">
-                    <label class="form-check-label" for="data_visualization">Data Visualization</label>
-                </div>
-
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="challenges" value="challenges" name="see_improved">
-                    <label class="form-check-label" for="challenges">Challenges</label>
-                </div>
-
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="open_source_community" value="open_source_community" name="see_improved">
-                    <label class="form-check-label" for="open_source_community">Open Source Community</label>
-                </div>
-
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="gitter" value="gitter" name="see_improved">
-                    <label class="form-check-label" for="gitter">Gitter help rooms</label>
-                </div>
-
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="videos" value="videos" name="see_improved">
-                    <label class="form-check-label" for="videos">Videos</label>
-                </div>
-
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="city_meetups" value="city_meetups" name="see_improved">
-                    <label class="form-check-label" for="city_meetups">City Meetups</label>
-                </div>
-
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="wiki" value="wiki" name="see_improved">
-                    <label class="form-check-label" for="wiki">Wiki</label>
-                </div>
-
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="forum" value="forum" name="see_improved">
-                    <label class="form-check-label" for="forum">Forum</label>
-                </div>
-
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="additional_courses" value="additional_courses" name="see_improved">
-                    <label class="form-check-label" for="additional_courses">Additional Courses</label>
-                </div>
-            </div>
-
-            <div class="form-group">
-                <label for="suggestions">Any comments or suggestions?</label>
-                <textarea class="form-control" id="suggestions" rows="5" placeholder="Enter your comment here..." required oninvalid="this.setCustomValidity('Please enter your suggestions')" oninput="setCustomValidity('')"></textarea>
-            </div>
-
-            <input type="submit" class="btn btn-primary"
-                value="<?= !empty($_GET['update'])? 'Update Data' : 'Save Data'; ?>">
         </div>
-    </form>
+    </div>
 
-    <div class="container">
+    <div class="container-fluid mb-4">
         <div class="card">
             <h5 class="card-header">All FCC Survey</h5>
             
@@ -170,7 +172,10 @@
 
                     <tbody>
                         <tr>
-                            <td>Donna Snider</td>
+                            <td>
+                                <a href=""><i class="far fa-edit"></i></a>
+                                <a href=""><i class="far fa-trash-alt"></i></a>
+                            </td>
                             <td>Donna Snider</td>
                             <td>Customer Support</td>
                             <td>New York</td>
