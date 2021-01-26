651fe3031a1920fac84ccab31411a1ddfe9cc40a
Sourab Hossain <sourabhossain.fci@gmail.com>
2021-01-26 20:00:35 +0600

    table data show

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php 
+
+include('db.php');
+
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -10,7 +16,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="css/dataTables.bootstrap4.min.css">
     <link rel="stylesheet" href="css/all.min.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
@@ -158,33 +164,55 @@
                 <table id="dataTable" class="table table-striped table-bordered" style="width:100%">
                     <thead>
                         <tr>
-                            <th>Action</th>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Age</th>
-                            <th>Current Status</th>
-                            <th>Recommend Friend</th>
-                            <th>Favorite Feature</th>
-                            <th>See Improved</th>
-                            <th>Suggestions</th>
+                            <th class="text-center" width="8%">Action</th>
+                            <th class="text-center" width="12%">Name</th>
+                            <th class="text-center" width="12%">Email</th>
+                            <th class="text-center" width="8%">Age</th>
+                            <th class="text-center" width="10%">Current Status</th>
+                            <th class="text-center" width="15%">Recommend Friend</th>
+                            <th class="text-center" width="10%">Favorite Feature</th>
+                            <th class="text-center" width="13%">See Improved</th>
+                            <th class="text-center" width="12%">Suggestions</th>
                         </tr>
                     </thead>
 
                     <tbody>
-                        <tr>
-                            <td>
-                                <a href=""><i class="far fa-edit"></i></a>
-                                <a href=""><i class="far fa-trash-alt"></i></a>
+
+                        <?php 
+                        
+                            $query = $conn->prepare("SELECT * FROM `registration` ORDER BY `id` ASC "); 
+                            $query->execute();
+                            $fetch_list = $query->fetchAll(PDO::FETCH_ASSOC);
+                            foreach($fetch_list AS $fetch) {
+
+                        ?>    
+
+                        <tr <?= (!empty($_GET['id']) && $fetch['id'] == $_GET['id'])? 'style="background: #1f2d3d; color: #ffffff;"' : '' ?>>
+                            <td  class="text-center">
+                                <a class="btn btn-info btn-sm" href="index.php?update=update&id=<?=$fetch['id'];?>">
+                                    <i class="far fa-edit"></i>
+                                </a>
+                                
+                                <a class="btn btn-danger btn-sm" href="javascript:delete_confirm('<?=$fetch['id'];?>');">
+                                    <i class="far fa-trash-alt"></i>
+                                </a>
                             </td>
-                            <td>Donna Snider</td>
-                            <td>Customer Support</td>
-                            <td>New York</td>
-                            <td>27</td>
-                            <td>2011/01/25</td>
-                            <td>$112,000</td>
-                            <td>$112,000</td>
-                            <td>$112,000</td>
+
+                            <td class="text-center"><?= $fetch['name']; ?></td>
+                            <td class="text-center"><?= $fetch['email']; ?></td>
+                            <td class="text-center"><?= $fetch['age']; ?></td>
+                            <td class="text-center"><?= $fetch['current_role']; ?></td>
+                            <td class="text-center"><?= $fetch['recommend_friend']; ?></td>
+                            <td class="text-center"><?= $fetch['favorite_feature']; ?></td>
+                            <td class="text-center"><?= $fetch['suggestions']; ?></td>
+                            <td class="text-center"><?= $fetch['see_improved']; ?></td>
                         </tr>
+
+                        <?php
+
+                            }
+
+                        ?>
                     </tbody>
                 </table>
             </div>
@@ -197,8 +225,8 @@
 
     <!-- JavaScript -->
     <script src="js/jquery-3.5.1.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap4.min.js"></script>
+    <script src="js/jquery.dataTables.min.js"></script>
+    <script src="js/dataTables.bootstrap4.min.js"></script>
     <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/script.js"></script>
